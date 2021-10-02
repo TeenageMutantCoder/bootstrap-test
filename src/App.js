2ac885554b522bbe0d6616b13b9609557d123a91
Stevon Wright <stevonisepic@gmail.com>
2021-10-02 11:50:14 -0400

    Added basic site structure to code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Header from "./header";
+import Hero from "./hero";
+import About from "./about";
+import Skills from "./skills";
+import Projects from "./projects";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
